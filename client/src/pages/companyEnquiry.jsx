f8fa8d1fa20efda6bc755c9d077c729dedc9caaf
Prakash Malwad <prakashmalwad8778999020@gmail.com>
2024-09-25 00:47:39 +0530

    Update companyEnquiry.jsx

--- client/src/pages/companyEnquiry.jsx
+++ client/src/pages/companyEnquiry.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import Navbar from '../components/Navbar';
+import Navbar from '../components/navbar';
 
 const CompanyEnquiryForm = () => {
   const [formData, setFormData] = useState({
