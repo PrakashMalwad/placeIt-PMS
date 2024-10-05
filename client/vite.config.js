fc86844248147bd2fe77ca710ff9f96e8583e924
Prakash Malwad <prakashmalwad8778999020@gmail.com>
2024-10-05 13:38:34 +0530

    build resolve

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['react-pdf', 'react-pdf/dist/esm/entry.webpack'],
-},
+  build: {
+    outDir: 'build', 
+  },
 }
+
 )
